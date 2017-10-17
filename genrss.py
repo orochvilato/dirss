032105e9c305abaacc02018ca2c52c555f1fee70
Olivier ROCH VILATO <olivierrochvilato@yahoo.com>
2017-10-17 07:13:41 -0400

    keepmsg

--- genrss.py
+++ genrss.py
@@ -9,12 +9,35 @@ import html
 from string import Template
 import jinja2
 import os
-channels = {'actualites.xml':{'id':'256392899720249344','label':'Actualités'},
-            'reseaux_sociaux.xml':{'id':'293416436620197889','label':'Réseaux sociaux'}}
+channels = {'actu_fi.xml':{'id':'256392899720249344','label':'Actualités FI','fic':'actu_fi.csv'},
+            'actu_generales.xml':{'id':'369220132121083904','label':'Actualités Autres','fic':'actu_generales.csv'},
+            'reseaux_sociaux.xml':{'id':'293416436620197889','label':'Réseaux sociaux','fic':'actu_rs.csv'}}
 
 
+#with open(inputFile, "r") as f1:
+#    last_line = f1.readlines()[-1]
 rss_length=15
 
+def extract_text(html):
+    import re
+    #import urllib.request
+    from bs4 import BeautifulSoup
+
+    #html = urllib.request.urlopen('http://bgr.com/2014/10/15/google-android-5-0-lollipop-release/')
+    soup = BeautifulSoup(html)
+    data = soup.findAll(text=True)
+
+    def visible(element):
+        if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+            return False
+        elif re.match('<!--.*-->', str(element.encode('utf-8'))):
+            return False
+        elif not element.parent.name in ['p','title']:
+            return False
+        return True
+
+    result = filter(visible, data)
+    return ' '.join(list(result))
 
 client = discord.Client()
 from jinja2 import Environment, PackageLoader, select_autoescape,FileSystemLoader
@@ -44,9 +67,15 @@ async def on_ready():
 
     for flux in channels.keys():
         channel = client.get_channel(channels[flux]['id'])
+        f = open(channels[flux]['fic'],'w+')
         items = []
         async for msg in client.logs_from(channel,limit=rss_length):
             links = re.search(r'(https?:[^ \n><]+)',msg.content)
+            tags = re.search(r'[ \n>]*#([^ \n<>]+)',msg.content)
+            if tags:
+                tags = ','.join(tags.groups())
+            else:
+                tags = ''
 
             if links:
                 for link in links.groups():
@@ -56,6 +85,7 @@ async def on_ready():
                     except:
                         c = r.content.decode(r.encoding)
 
+                    #print(extract_text(c))
                     site = getmeta(c,"og:site_name")
                     title = getmeta(c,"og:title")
                     description = getmeta(c,"og:description")
@@ -69,7 +99,7 @@ async def on_ready():
                              site=site,
                              image=image,
                              description=getmeta(c,"og:description")))
-
+                    f.write(';'.join([msg.id,msg.author if isinstance(msg.author,str) else msg.author.name ,tags,link,date,site,description,image]+['\n']))
         with open('html/%s' % flux,'wb') as f:
             f.write(env.get_template('rss.html').render(items=items,
                                                         link="https://discordapp.com/channels/254215611264008192/"+channels[flux]['id'],

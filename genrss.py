093925370c80689d0b6487428fae8ae415ffc447
Olivier ROCH VILATO <olivier.rochvilato@chilbp.fr>
2017-10-17 09:03:26 -0400

    Ajout génération xlsx

--- genrss.py
+++ genrss.py
@@ -9,14 +9,14 @@ import html
 from string import Template
 import jinja2
 import os
-channels = {'actu_fi.xml':{'id':'256392899720249344','label':'Actualités FI','fic':'actu_fi.csv'},
-            'actu_generales.xml':{'id':'369220132121083904','label':'Actualités Autres','fic':'actu_generales.csv'},
-            'reseaux_sociaux.xml':{'id':'293416436620197889','label':'Réseaux sociaux','fic':'actu_rs.csv'}}
+channels = {'actu_fi.xml':{'id':'256392899720249344','label':'Actualités FI','fic':'actu_fi.xlsx'},
+            'actu_generales.xml':{'id':'369220132121083904','label':'Actualités Autres','fic':'actu_generales.xlsx'},
+            'reseaux_sociaux.xml':{'id':'293416436620197889','label':'Réseaux sociaux','fic':'actu_rs.xlsx'}}
 
 
 #with open(inputFile, "r") as f1:
 #    last_line = f1.readlines()[-1]
-rss_length=15
+rss_length=30
 
 def extract_text(html):
     import re
@@ -47,6 +47,8 @@ env = Environment(
 )
 
 
+from openpyxl import Workbook,load_workbook
+
 
 client = discord.Client()
 
@@ -66,10 +68,23 @@ async def on_ready():
     print('------')
 
     for flux in channels.keys():
+
         channel = client.get_channel(channels[flux]['id'])
-        f = open(channels[flux]['fic'],'w+')
+        try:
+            wb = load_workbook(channels[flux]['fic'])
+            ws = wb.active
+        except:
+            wb = Workbook()
+            ws = wb.active
+            ws.append(['ID','Auteur','Tags','Lien','Date','Titre','Site','Description','Image'])
+
+        lastid = max([v.value for v in ws['A'][1:]]) if (len(ws['A'])>1) else 'NOPE'
+        print(lastid)
         items = []
         async for msg in client.logs_from(channel,limit=rss_length):
+            if msg.id == lastid:
+                print('bingo!')
+                break
             links = re.search(r'(https?:[^ \n><]+)',msg.content)
             tags = re.search(r'[ \n>]*#([^ \n<>]+)',msg.content)
             if tags:
@@ -87,19 +102,26 @@ async def on_ready():
 
                     #print(extract_text(c))
                     site = getmeta(c,"og:site_name")
-                    title = getmeta(c,"og:title")
+                    titre = getmeta(c,"og:title")
                     description = getmeta(c,"og:description")
 
                     date = email.utils.format_datetime(msg.timestamp)
                     image = getmeta(c,'og:image') or getmeta(c,'og:image:url')
 
-                    items.append(dict(titre=getmeta(c,"og:title"),
-                             link=link,
-                             date=date,
-                             site=site,
-                             image=image,
-                             description=getmeta(c,"og:description")))
-                    f.write(';'.join([msg.id,msg.author if isinstance(msg.author,str) else msg.author.name ,tags,link,date,site,description,image]+['\n']))
+                    ws.append([msg.id,msg.author if isinstance(msg.author,str) else msg.author.name ,tags,link,msg.timestamp,titre,site,description,image])
+
+        wb.save('html/%s' % channels[flux]['fic'])
+        items = []
+        for r in list(ws.iter_rows())[1:][-30:]:
+
+            date = email.utils.format_datetime(r[4].value)
+            items.append(dict(titre=r[5].value,
+                         link=r[3].value,
+                         date=date,
+                         site=r[6].value,
+                         image=r[8].value,
+                         description=r[7].value))
+
         with open('html/%s' % flux,'wb') as f:
             f.write(env.get_template('rss.html').render(items=items,
                                                         link="https://discordapp.com/channels/254215611264008192/"+channels[flux]['id'],
